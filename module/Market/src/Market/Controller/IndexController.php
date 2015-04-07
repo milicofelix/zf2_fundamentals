54504fc85f26edd1bc1d0c3e423049d16f459a6e
Adriano Freitas <milicofelix@gmail.com>
2015-04-06 23:20:25 -0300

    M5Ex1-A-B-C

---
+++ module/Market/src/Market/Controller/IndexController.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Description of IndexController
+ *
+ * @author adriano
+ */
+
+namespace Market\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+class IndexController extends AbstractActionController {
+
+    public function indexAction() {
+        return new ViewModel();
+    }
+
+}
